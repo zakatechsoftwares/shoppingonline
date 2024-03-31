5839f3161688c2202bcd277debd22f463ca79199
Saka Adeyemi Shittu <zakatechsoftware@gmail.com>
2024-03-31 18:14:08 +0100

    bootstrap-icon module installed

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+
 import "../node_modules/bootstrap/dist/css/bootstrap.css";
 import "../node_modules/bootstrap-icons/font/bootstrap-icons.css";
 import ReactDOM from "react-dom/client";

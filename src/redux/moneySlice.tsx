497ff9577f35c0f6ae47a7e29aed3d802a11d7d7
Saka Adeyemi Shittu <zakatechsoftware@gmail.com>
2024-03-31 14:52:20 +0100

    Moneyslice reducers commented out

--- src/redux/moneySlice.tsx
+++ src/redux/moneySlice.tsx
@@ -1,4 +1,6 @@
-import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import {
+  createSlice, //, PayloadAction
+} from "@reduxjs/toolkit";
 
 export type MoneyType = {
   currencyUnit: string;
@@ -18,14 +20,14 @@ export const MoneySlice = createSlice({
   name: "money",
   initialState,
   reducers: {
-    moneyCalc: (state, action: PayloadAction<MoneyType>) =>
-      (state = action.payload),
-    budgetedChanged: (state, action) => {
-      state.budgeted = action.payload;
-    },
-    currencyUnitChanged: (state, action: PayloadAction<string>) => {
-      state.currencyUnit = action.payload;
-    },
+    // moneyCalc: (state, action: PayloadAction<MoneyType>) =>
+    //   (state = action.payload),
+    // budgetedChanged: (state, action) => {
+    //   state.budgeted = action.payload;
+    // },
+    // currencyUnitChanged: (state, action: PayloadAction<string>) => {
+    //   state.currencyUnit = action.payload;
+    // },
   },
 });
 
